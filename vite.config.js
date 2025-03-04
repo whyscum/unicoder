06f8830d6fab9f16d6e0f209316e97c962a0c6a0
Maxim Danilov <whyscum@gmail.com>
2025-03-04 19:22:39 +0300

    Integrated TypeScript and Styled-Comp

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/unicoder/"
+  base: "/unicoder/",
+  server: {
+    open: true,
+    port: 3001,
+  },
 })
