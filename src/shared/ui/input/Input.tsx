931a417bc4039c1bfe765a1b4df2355e898c90f6
Maxim Danilov <whyscum@gmail.com>
2025-03-14 21:47:18 +0300

    Integrated TypeScript and Styled-Comp

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import {MyInputProps} from "../../interfaces/interfaces";
+// import {MyInputProps} from "../../interfaces/interfaces";
 import {MyInputStyled} from "../../styled/styled-components";
 
-const Input: React.FC<MyInputProps> = React.forwardRef<HTMLInputElement, MyInputProps>((props, ref) => {
+const Input: (props: any, ref: any) => React.JSX.Element =((props: any , ref: any) => {
     return (
         <MyInputStyled ref={ref} {...props}/>
     )
 })
-
-Input.displayName = 'MyInput'
 
 export default Input;
